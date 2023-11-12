fbb7c7b336bfec158ba98ac33c154644f233e152
Daniel Yang <shu.minyang@hotmail.com>
2023-11-12 12:35:02 -0500

    feat: finishing get company query

--- api/company/index.ts
+++ api/company/index.ts
@@ -4,6 +4,10 @@ import { API_URL } from "..";
 
 axios.defaults.baseURL = API_URL;
 
+export const COMPANY_KEYS = {
+  getCompanies: "getCompanies",
+};
+
 export const CompanyTypeEnum = z.enum([
   "Financeira",
   "Tecnologia",
@@ -15,8 +19,21 @@ export const CreateCompany = z.object({
   type: z.optional(CompanyTypeEnum).default("Consultoria"),
 });
 
+const GetCompany = z.object({
+  id: z.string(),
+  name: z.string(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+  type: z.string(),
+});
+
 export type CreateCompanyType = z.infer<typeof CreateCompany>;
+type GetCompanyType = z.infer<typeof GetCompany>;
 
 export async function createCompanyApi(data: CreateCompanyType) {
   return axios.post("/company", data).then((res) => res.data);
+}
+
+export async function getCompanyApi(): Promise<GetCompanyType[]> {
+  return axios.get("/company").then((res) => res.data);
 }
--- components/templates/Template.tsx
+++ components/templates/Template.tsx
@@ -1,32 +1,23 @@
-import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
-import {
-  EditIcon,
-  HomeIcon,
-  MoonIcon,
-  Plus,
-  PlusCircleIcon,
-} from "lucide-react";
+import { EditIcon, HomeIcon, PlusCircleIcon } from "lucide-react";
 
 import { EditNameCompany } from "@/components/forms/EditCompany";
 import { SubMenu } from "@/components/subMenu";
 import { TopMenu } from "@/components/topMenu";
 import { DeleteCompany } from "@/components/forms/DeleteCompany";
 import { EditLogoCompany } from "@/components/forms/EditLogoCompany";
-import { useContext } from "react";
-import { GlobalContext } from "@/contexts/GlobalContext";
-import { MOCK_USER } from "@/mocks/user";
 import Link from "next/link";
 import Image from "next/image";
 import { Dialog } from "../dialog";
 import { useParams } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import { COMPANY_KEYS, getCompanyApi } from "@/api/company";
 
 export const AppTemplate = ({ children }: React.PropsWithChildren) => {
   const selectedCompany = useParams();
-  const findSpecificCompany = MOCK_USER.companies.find(
-    (company) => company.company_id === selectedCompany?.company_id
-  );
-
-  const { selectedCompany: hoverCompany } = useContext(GlobalContext);
+  const { data } = useQuery({
+    queryKey: [COMPANY_KEYS.getCompanies],
+    queryFn: getCompanyApi,
+  });
 
   const FORMS = {
     edit_company: EditNameCompany,
@@ -44,7 +35,7 @@ export const AppTemplate = ({ children }: React.PropsWithChildren) => {
         <div className="w-full flex flex-col">
           <Image
             alt="Image"
-            src={findSpecificCompany?.company_logo || "/isotipo_png.png"}
+            src="/isotipo_png.png"
             className="w-14 h-[58px] min-h-[54px] min-w-[54px] p-3 border-b border-default dark:border-defaultdark flex items-center justify-center"
             width={56}
             height={58}
@@ -71,68 +62,23 @@ export const AppTemplate = ({ children }: React.PropsWithChildren) => {
                 Content={FORMS["edit_company"]}
               />
             </div>
-            {findSpecificCompany?.company_products.map((product) => (
+            {data?.map(({ id, name }) => (
               <Link
-                key={product.product_id}
-                href={`product/${product.product_id}`}
+                key={id}
+                href={`company/${id}`}
                 className="group flex items-center justify-center w-10 h-10 rounded-md hover:bg-[#e6e8eb] dark:hover:bg-[#2f2f2f] dark:text-[gray] overflow-hidden p-1 group"
               >
                 <div className="invisible group-hover:visible absolute text-primary mt-[-30px] ml-[-30px]">
                   <EditIcon size={16} />
                 </div>
                 <Image
-                  src={product.product_logo}
-                  alt={product.product_name}
+                  src="/isotipo_png.png"
+                  alt={name}
                   width={30}
                   height={30}
                 />
               </Link>
-            )) ||
-              MOCK_USER.companies.map((company) => (
-                <Link
-                  key={company.company_id}
-                  href={`/company/${company.company_id}`}
-                  className={`group flex items-center justify-center w-10 h-10 rounded-md hover:bg-[#e6e8eb] dark:hover:bg-[#2f2f2f] dark:text-[gray] overflow-hidden p-1 group ${
-                    company?.company_id === hoverCompany?.company_id &&
-                    "bg-[#e6e8eb]"
-                  }`}
-                >
-                  <div className="invisible group-hover:visible absolute text-primary mt-[-30px] ml-[-30px]">
-                    <DropdownMenu.Root>
-                      <DropdownMenu.Trigger>
-                        <EditIcon size={16} />
-                      </DropdownMenu.Trigger>
-                      <DropdownMenu.Content className="bg-foreground flex flex-col w-max max-w-[250px] gap-2 border border-default dark:border-defaultdark rounded-md overflow-hidden shadow-md drop-shadow-lg">
-                        <DropdownMenu.Item className="w-full h-7 flex justify-between items-center gap-2 hover:bg-primary px-2 py-3">
-                          <Plus size={20} />
-                          <span className="flex justify-start w-full text-xs">
-                            Criar
-                          </span>
-                        </DropdownMenu.Item>
-                      </DropdownMenu.Content>
-                    </DropdownMenu.Root>
-                    {/* <Dialog
-                  title={`Editar ${company.company_name}`}
-                  description={`Edite a ${company.company_name}`}
-                  Trigger={
-                    <Settings
-                    fill="rgb(150 162 242)"
-                    stroke="#2f2f2f"
-                    size={15}
-                    />
-                  }
-                  Content={EditNameCompany}
-                /> */}
-                  </div>
-                  {/* <ContextMenu /> */}
-                  <Image
-                    src={company.company_logo}
-                    alt={company.company_name}
-                    width={56}
-                    height={56}
-                  />
-                </Link>
-              ))}
+            ))}
           </div>
         </div>
       </div>
