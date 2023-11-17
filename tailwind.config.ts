3cdb1d5f765cbbfdcec0ab570f2564ebae59b6ef
Daniel Yang <shu.minyang@hotmail.com>
2023-11-17 07:17:51 -0500

    LOL

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,19 +7,20 @@ const config: Config = {
   ],
   daisyui: {
     themes: [
-      {
-        mytheme: {
-          primary: "#00c9ff",
-          secondary: "#00bdc0",
-          accent: "#008eff",
-          neutral: "#080f0f",
-          "base-100": "#fcfcfc",
-          info: "#00a3ff",
-          success: "#00b784",
-          warning: "#e57500",
-          error: "#d20028",
-        },
-      },
+      "valentine",
+      // {
+      //   mytheme: {
+      //     primary: "#00c9ff",
+      //     secondary: "#00bdc0",
+      //     accent: "#008eff",
+      //     neutral: "#080f0f",
+      //     "base-100": "#fcfcfc",
+      //     info: "#00a3ff",
+      //     success: "#00b784",
+      //     warning: "#e57500",
+      //     error: "#d20028",
+      //   },
+      // },
     ],
   },
   plugins: [require("daisyui")],
