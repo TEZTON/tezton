d6cd2e5c68d7d69b449a5e1234409d30763e7b8f
GHAlmeidaDev <gaabriel.hudson11@gmail.com>
2024-01-08 11:08:09 -0300

    fix

--- drizzle/0000_wealthy_yellow_claw.sql
+++ drizzle/0000_wealthy_yellow_claw.sql
@@ -11,7 +11,6 @@ CREATE TABLE `companies` (
 CREATE TABLE `deliverables` (
 	`id` text PRIMARY KEY NOT NULL,
 	`name` text NOT NULL,
-	`description` text,
 	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	`updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	`functionality_id` text NOT NULL,
---
+++ drizzle/0008_short_rumiko_fujikawa.sql
@@ -0,0 +1,3 @@
+ALTER TABLE deliverables ADD `description` text;
+ALTER TABLE deliverable_diagram_boundries ADD `description` text;
+ALTER TABLE deliverable_diagram_nodes ADD `description` text;
